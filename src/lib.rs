c62c32590d4e900eed5868948870dbb35ce55a38
thekuwayama <thekuwayama@gmail.com>
2020-04-05 05:30:14 +0900

    clean rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -8,20 +8,14 @@ pub fn entrypoint(kvs: &mut KVStore, req: &Request<Vec<u8>>) -> Response<Vec<u8>
 
     if req.method().as_str() == "POST" && !path.is_empty() {
         kvs.insert(&path, req.body());
-        Response::builder()
-            .status(200)
-            .body(b"".to_vec())
-            .unwrap()
+        Response::builder().status(200).body(b"".to_vec()).unwrap()
     } else if req.method().as_str() == "GET" && !path.is_empty() {
         Response::builder()
             .status(200)
             .body(kvs.get(&path).unwrap_or(b"".to_vec()))
             .unwrap()
     } else {
-        Response::builder()
-            .status(400)
-            .body(b"".to_vec())
-            .unwrap()
+        Response::builder().status(400).body(b"".to_vec()).unwrap()
     }
 }
 
