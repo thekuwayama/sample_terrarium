93cc415935c90ac7fa1f4023c3a987fc677185ec
thekuwayama <thekuwayama@gmail.com>
2020-04-05 04:55:54 +0900

    using http_guest::kvstore::KVStore

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,28 @@
 #[macro_use]
 extern crate http_guest;
 
-use http_guest::{Request, Response};
+use http_guest::{KVStore, Request, Response};
 
-pub fn user_entrypoint(_req: &Request<Vec<u8>>) -> Response<Vec<u8>> {
-    Response::builder()
-        .status(200)
-        .body("Hello, world!".as_bytes().to_owned())
-        .unwrap()
+pub fn entrypoint(kvs: &mut KVStore, req: &Request<Vec<u8>>) -> Response<Vec<u8>> {
+    let path = req.uri().to_string();
+
+    if req.method().as_str() == "POST" && !path.is_empty() {
+        kvs.insert(&path, req.body());
+        Response::builder()
+            .status(200)
+            .body(b"".to_vec())
+            .unwrap()
+    } else if req.method().as_str() == "GET" && !path.is_empty() {
+        Response::builder()
+            .status(200)
+            .body(kvs.get(&path).unwrap_or(b"".to_vec()))
+            .unwrap()
+    } else {
+        Response::builder()
+            .status(400)
+            .body(b"".to_vec())
+            .unwrap()
+    }
 }
 
-guest_app!(user_entrypoint);
+guest_app_kvs!(entrypoint);
